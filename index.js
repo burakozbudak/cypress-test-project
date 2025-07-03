9e637757c93544a2171d576c08bc0ad3024122bb
Burak Özbudak <burakozbudak25@hacettepe.edu.tr>
2025-07-03 20:11:34 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+function cypressTestProjesi() {
+  const checklist = {
+    //github repo adresini gir: Örn: https://github.com/code2work/s7-form-validations
+    project_url: "https://github.com/burakozbudak/cypress-test-project",
+  };
+  return checklist;
+}
